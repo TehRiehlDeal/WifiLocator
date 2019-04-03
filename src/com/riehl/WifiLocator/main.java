25928345d33ef24f88884a625e54e51ea469ff4d
Kevin Riehl <kevinriehl@gmail.com>
2019-04-03 09:55:30 -0700

    Removed debug code

--- src/com/riehl/WifiLocator/main.java
+++ src/com/riehl/WifiLocator/main.java
@@ -1,16 +1,15 @@
 package com.riehl.WifiLocator;
 
-import com.riehl.WifiLocator.NetorkDiscovery.NetworkDiscovery;
+import com.riehl.WifiLocator.NetorkDiscovery.MacNetworkDiscovery;
 
 public class main {
 
     public static void main(String[] args) {
 
         String os = System.getProperty("os.name").toLowerCase();
-        System.out.println(os);
         if (os.contains("mac")){
             for (int i = 0; i < 10; i++) {
-                NetworkDiscovery.readNetworks();
+                MacNetworkDiscovery.readNetworks();
             }
         } else {
             System.out.println("Sorry your Operating System is not supported yet.");
