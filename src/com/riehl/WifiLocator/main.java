8c6f2c9d0ca061172e727fe0b99cd22e206459d1
Kevin Riehl <kevinriehl@gmail.com>
2019-04-03 09:52:15 -0700

    Added check for supported OS

--- src/com/riehl/WifiLocator/main.java
+++ src/com/riehl/WifiLocator/main.java
@@ -6,10 +6,14 @@ public class main {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 10; i++) {
-            NetworkDiscovery.readNetworks();
-
+        String os = System.getProperty("os.name").toLowerCase();
+        System.out.println(os);
+        if (os.contains("mac")){
+            for (int i = 0; i < 10; i++) {
+                NetworkDiscovery.readNetworks();
+            }
+        } else {
+            System.out.println("Sorry your Operating System is not supported yet.");
         }
-
     }
 }
