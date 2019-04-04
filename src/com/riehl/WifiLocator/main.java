a9f42c0dd1ea3da093ec2c50f89c0977db2bc57d
Kevin Riehl <kevinriehl@gmail.com>
2019-04-04 11:44:04 -0700

    Made the loop check for and use the new network name variable

--- src/com/riehl/WifiLocator/main.java
+++ src/com/riehl/WifiLocator/main.java
@@ -4,12 +4,18 @@ import com.riehl.WifiLocator.NetorkDiscovery.MacNetworkDiscovery;
 
 public class main {
 
+    static String network = "";
     public static void main(String[] args) {
 
         String os = System.getProperty("os.name").toLowerCase();
         if (os.contains("mac")){
-            for (int i = 0; i < 10; i++) {
-                MacNetworkDiscovery.readNetworks();
+            while(true) {
+                if (!network.isEmpty()){
+                    MacNetworkDiscovery.readNetworks(network);
+                } else {
+                    network = MacNetworkDiscovery.readNetworks("");
+                }
+
             }
         } else {
             System.out.println("Sorry your Operating System is not supported yet.");
